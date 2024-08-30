5df44cc112fa70c062a5745712925038c92bff91
Sergey Zholudev <serg.zholudev@gmail.com>
2024-08-30 19:49:53 +0300

    App: lists, are added

---
+++ superlists/functional_tests.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    """Тест нового посетителя"""
+
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: можно начать список и продолжить его позже"""
+        # Эдит слышала про крутое новое онлайн-приложение со
+        # списком неотложных дел. Она решает оценить его домашнюю
+        # страницу
+        self.browser.get('http://localhost:8000')
+
+        # Она видит, что заголовок и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+        # Ей сразу же предлагается ввести элемент списка
+
+
+if __name__ == '__main__':
+    unittest.main()
